60c47253ffc70425e099e6573cd61aabd9b710ad
Sagar <deshpandesagar187@gmail.com>
2024-02-15 14:17:25 +0530

    Added files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Login from './components/Login';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Logout from './components/Logout';
+
+function App() {
+
+  const user = useSelector(selectUser);
+
+  return (
+    <div className="App">
+      {user ? <Logout /> : <Login />}
+    </div>
+  );
+}
+
+export default App;
